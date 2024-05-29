3ed69f07954ed7d9148ca7eeaf169f038ca0e865
Noé <noefarese@gmx.ch>
2024-05-29 18:39:27 +0200

    start with alert on home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {
   IonHeader,
   IonToolbar,
@@ -7,8 +7,9 @@ import {
   IonButton,
   IonList,
   IonLabel,
-  IonItem, IonCol, IonRow, IonListHeader, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonGrid
+  IonItem, IonCol, IonRow, IonListHeader, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonGrid, AlertController
 } from '@ionic/angular/standalone';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-home',
@@ -19,14 +20,46 @@ import {
 })
 export class HomePage {
   rows = [
-    { name: 'Row 1', date: '123', emoji1: '456', emoji2: '456' },
-    { name: 'Row 2', date: '123', emoji1: '456', emoji2: '456' },
-    { name: 'Row 3', date: '123', emoji1: '456', emoji2: '456' },
-    { name: 'Row 4', date: '123', emoji1: '456', emoji2: '456' },
-    { name: 'Row 5', date: '123', emoji1: '456', emoji2: '456' },
-    { name: 'Row 6', date: '123', emoji1: '456', emoji2: '456' },
-    { name: 'Row 7', date: '123', emoji1: '456', emoji2: '456' },
+    {name: 'Row 1', date: '123', emoji1: '456', emoji2: '456'},
+    {name: 'Row 2', date: '123', emoji1: '456', emoji2: '456'},
+    {name: 'Row 3', date: '123', emoji1: '456', emoji2: '456'},
+    {name: 'Row 4', date: '123', emoji1: '456', emoji2: '456'},
+    {name: 'Row 5', date: '123', emoji1: '456', emoji2: '456'},
+    {name: 'Row 6', date: '123', emoji1: '456', emoji2: '456'},
+    {name: 'Row 7', date: '123', emoji1: '456', emoji2: '456'},
   ];
 
-  constructor() {}
+  private alertController = inject(AlertController)
+  private router = inject(Router)
+
+  async startNewSchnitzelJagd() {
+    const alert = await this.alertController.create({
+      header: 'Enter Name',
+      inputs: [
+        {
+          name: 'name',
+          type: 'text',
+          placeholder: 'Your Name'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        },
+        {
+          text: 'OK',
+          handler: (data) => {
+            console.log('Confirm Ok', data);
+            this.router.navigate(['/'], {queryParams: {name: data.name}});
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 }
