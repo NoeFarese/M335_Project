a1272ae5c43e88029d96b590931994ea54f19024
Noé <110814367+NoeFarese@users.noreply.github.com>
2024-06-01 11:24:35 +0200

    use task component on distance task page

--- src/app/distance/distance.page.ts
+++ src/app/distance/distance.page.ts
@@ -1,33 +1,16 @@
-import {Component, inject} from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import {
-  IonButton,
-  IonCard,
-  IonCardContent,
-  IonCardHeader, IonCardTitle,
-  IonContent,
-  IonHeader,
-  IonTitle,
-  IonToolbar
-} from '@ionic/angular/standalone';
-import {Router} from "@angular/router";
+import {Component} from '@angular/core';
+import {TaskComponent} from "../task/task.component";
 
 @Component({
   selector: 'app-distance',
   templateUrl: './distance.page.html',
   styleUrls: ['./distance.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonButton]
+  imports: [TaskComponent]
 })
 export class DistancePage {
   isTaskDone: boolean = false;
-  progressValue: number = 50;
-  private router = inject(Router)
 
   constructor() { }
 
-  navigateToNextTask(): void {
-    this.router.navigate([''])
-  }
 }
