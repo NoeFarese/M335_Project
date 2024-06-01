45b9f4e7c306061a7bf5cefb8faa628ee72e43b0
Noé <110814367+NoeFarese@users.noreply.github.com>
2024-06-01 11:24:52 +0200

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,16 +6,28 @@ export const routes: Routes = [
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
   },
   {
+    path: 'geolocation',
+    loadComponent: () => import('./geolocation/geolocation.page').then( m => m.GeolocationPage)
+  },
+  {
+    path: 'distance',
+    loadComponent: () => import('./distance/distance.page').then( m => m.DistancePage)
+  },
+  {
+    path: 'qr-code',
+    loadComponent: () => import('./qr-code/qr-code.page').then( m => m.QRCodePage)
+  },
+  {
+    path: 'device-status',
+    loadComponent: () => import('./device-status/device-status.page').then( m => m.DeviceStatusPage)
+  },
+  {
+    path: 'finish',
+    loadComponent: () => import('./finish/finish.page').then( m => m.FinishPage)
+  },
+  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
-  },
-  {
-    path: 'geolocation',
-    loadComponent: () => import('./geolocation/geolocation.page').then( m => m.GeolocationPage)
-  },  {
-    path: 'distance',
-    loadComponent: () => import('./distance/distance.page').then( m => m.DistancePage)
   }
-
 ];
