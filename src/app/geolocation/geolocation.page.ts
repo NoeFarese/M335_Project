bf9338b63d83f3a5b47eb55b90b72c4a23307c61
Noé <110814367+NoeFarese@users.noreply.github.com>
2024-06-01 11:24:43 +0200

    use task component on geolocation task page

--- src/app/geolocation/geolocation.page.ts
+++ src/app/geolocation/geolocation.page.ts
@@ -1,33 +1,16 @@
-import {Component, inject} from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import {
-    IonButton,
-    IonCard, IonCardContent,
-    IonCardHeader, IonCardSubtitle,
-    IonCardTitle,
-    IonContent,
-    IonHeader,
-    IonTitle,
-    IonToolbar
-} from '@ionic/angular/standalone';
-import {Router} from "@angular/router";
+import {Component} from '@angular/core';
+import {TaskComponent} from "../task/task.component";
 
 @Component({
   selector: 'app-geolocation',
   templateUrl: './geolocation.page.html',
   styleUrls: ['./geolocation.page.scss'],
   standalone: true,
-    imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonButton]
+  imports: [TaskComponent]
 })
 export class GeolocationPage {
   isTaskDone: boolean = false;
-  progressValue: number = 25;
-  private router = inject(Router)
 
   constructor() { }
 
-  navigateToNextTask(): void {
-    this.router.navigate([''])
-  }
 }
