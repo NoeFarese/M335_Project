77996fb8ef092116bb64719a9adf21074ac5c11b
Noé <110814367+NoeFarese@users.noreply.github.com>
2024-06-01 11:22:51 +0200

    create task component

---
+++ src/app/task/task.component.ts
@@ -0,0 +1,29 @@
+import {Component, inject, Input} from '@angular/core';
+import {IonicModule} from "@ionic/angular";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.scss'],
+  imports: [
+    IonicModule
+  ],
+  standalone: true
+})
+export class TaskComponent {
+
+  @Input() title: string | undefined;
+  @Input() description: string | undefined;
+  @Input() availableTime: string | undefined;
+  @Input() progressValue: number | undefined;
+  @Input() isTaskDone: boolean | undefined;
+  @Input() nextRoute: string | undefined;
+  private router = inject(Router);
+
+  constructor() { }
+
+  navigateToNextTask() {
+    this.router.navigate([this.nextRoute]);
+  }
+}
