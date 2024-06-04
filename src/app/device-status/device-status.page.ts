ecccc230ddbce42460c9299014636e57ebb31c3f
Noé <110814367+NoeFarese@users.noreply.github.com>
2024-06-04 15:31:47 +0200

    delete unused import

--- src/app/device-status/device-status.page.ts
+++ src/app/device-status/device-status.page.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {TaskComponent} from "../task/task.component";
 import { Device } from "@capacitor/device";
 
