40ec10674823cf89b99a5abbfefd170e1751d224
Noah Feller <noah.feller@gmx.ch>
2024-06-05 13:34:34 +0200

    implemented distance task

--- src/app/distance/distance.page.ts
+++ src/app/distance/distance.page.ts
@@ -1,5 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {TaskComponent} from "../task/task.component";
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-distance',
@@ -8,9 +9,60 @@ import {TaskComponent} from "../task/task.component";
   standalone: true,
   imports: [TaskComponent]
 })
-export class DistancePage {
-  isTaskDone: boolean = false;
+export class DistancePage implements OnInit {
+  isTaskDone: boolean = true;
+  initialLon : number = 0;
+  initialLat : number = 0;
+  threshold : number = 20 // in meters
 
   constructor() { }
 
+  async ngOnInit() {
+    const coordinates = await Geolocation.getCurrentPosition({
+      enableHighAccuracy: true,
+    });
+
+    this.initialLat = coordinates.coords.latitude;
+    this.initialLon = coordinates.coords.longitude;
+
+    this.startTracking();
+  }
+
+  async startTracking() {
+    setInterval(async () => {
+      const coordinates = await Geolocation.getCurrentPosition({
+        enableHighAccuracy: true,
+      });
+
+      const currentLat = coordinates.coords.latitude;
+      const currentLon = coordinates.coords.longitude;
+      const distance = this.haversineDistance(this.initialLat, this.initialLon, currentLat, currentLon);
+
+      if (distance > this.threshold) {
+        this.isTaskDone = true;
+      }
+      console.log('Distance: ', distance);
+    }, 3000);
+  }
+
+  haversineDistance(lat1: number, lon1: number, lat2: number, lon2: number): number {
+    const R = 6371e3; // Earth's radius in meters
+    const lat1Rad = lat1 * (Math.PI / 180);
+    const lat2Rad = lat2 * (Math.PI / 180);
+
+    const deltaLat = (lat2 - lat1) * (Math.PI / 180);
+    const deltaLon = (lon2 - lon1) * (Math.PI / 180);
+
+    const a =
+    Math.sin(deltaLat / 2) * Math.sin(deltaLat / 2) +
+    Math.cos(lat1Rad) *
+      Math.cos(lat2Rad) *
+      Math.sin(deltaLon / 2) *
+      Math.sin(deltaLon / 2);
+
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const distance = R * c;
+
+    return distance;
+  }
 }
